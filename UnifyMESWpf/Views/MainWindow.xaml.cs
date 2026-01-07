ace815087b79d2b7d53ebf1de829e72c3a9e82a1
Hojung Kim <mireu9275@naver.com>
2026-01-07 17:31:45 +0900

    프로젝트 구조 재구성: MVVM 패턴 적용을 위한 폴더 생성 및 MainWindow 이동

--- UnifyMESWpf/Views/MainWindow.xaml.cs
+++ UnifyMESWpf/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UnifyMESWpf
+namespace UnifyMESWpf.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
